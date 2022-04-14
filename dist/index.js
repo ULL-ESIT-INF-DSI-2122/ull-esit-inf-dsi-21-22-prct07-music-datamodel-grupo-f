d94e6e6df95fd24991f0732ae2a342bdc2294b1c
Leonardo Cruz <alu0101233093@ull.edu.es>
2022-04-14 15:15:43 +0000

    arreglando gitignore

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const gestor_1 = require("./gestor");
-const database_1 = require("./database");
-let db = new database_1.DataBase();
-let app = new gestor_1.Gestor(db);
-app.promptUser();
-//# sourceMappingURL=index.js.map
--- dist/tests/clases.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("mocha");
-describe('Tests canciones', () => {
-    it('', () => {
-        let c;
-    });
-});
-//# sourceMappingURL=clases.spec.js.map
