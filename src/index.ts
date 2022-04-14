dfa896a6d0251ca951d6c109c0eb89f60288be02
Jose Orlando Nina Orellana <alu0101322308@ull.edu.es>
2022-04-14 14:45:18 +0000

    actualizar index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Gestor } from './gestor';
 import { DataBase } from './database';
+import { DataBaseManipulator } from './prompt'
+import inquirer from 'inquirer';
 
 let db = new DataBase();
-let app = new Gestor(db);
+let app = new DataBaseManipulator(db);
 app.promptUser();
